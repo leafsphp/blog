11bcbb55ed90d3bd012d46dfd59ad04c5eaf8abc
Michael Darko <mickdd22@gmail.com>
2021-12-29 18:10:46 +0000

    :tada: created base project

---
+++ .vitepress/config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  title: 'Leaf Blog',
+  description: 'The offical blog for Leaf PHP',
+  head: [
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  vite: {
+    build: {
+      minify: 'terser'
+    }
+  }
+})
---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="antialiased">
+    <div class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <nav class="flex justify-between items-center py-10 font-bold">
+        <a class="text-xl" href="/" aria-label="Leaf Blog">
+          <img
+            class="inline-block mr-2"
+            style="width: 36px; height: 36px"
+            alt="logo"
+            src="/logo-circle.png"
+          />
+          <span v-if="!isIndex" class="hidden md:inline">Leaf Blog</span>
+        </a>
+        <div class="text-base text-gray-500 leading-5">
+          <a href="#" style="margin-right:20px;" @click="toggleMode">Toggle Theme</a>
+
+          <a
+            class="hover:text-gray-700"
+            href="https://leafphp.dev"
+            target="_blank"
+            rel="noopener"
+            >leafphp.dev →</a
+          >
+        </div>
+      </nav>
+    </div>
+    <main class="max-w-3xl mx-auto px-4 sm:px-6 xl:max-w-5xl xl:px-0">
+      <Home v-if="isIndex" />
+      <Article v-else />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted } from 'vue'
+import { useRoute } from 'vitepress'
+import Home from './Home.vue'
+import Article from './Article.vue'
+
+let isDark = (window.localStorage["isDark"] || false);
+
+const route = useRoute()
+const isIndex = computed(() => route.path.replace(/index.html$/, '') === '/');
+
+onMounted(() => {
+  console.log('mounted', isDark);
+  applyMode();
+});
+
+const applyMode = () => {
+  const body = document.body;
+
+  console.log(isDark);
+
+  if (isDark === 'true' || isDark === true) {
+    body.classList.remove("-light");
+  } else {
+    body.classList.add("-light");
+  }
+};
+
+const toggleMode = () => {
+  window.localStorage["isDark"] = !isDark;
+  isDark = !isDark;
+
+  console.log(isDark, window.localStorage["isDark"]);
+
+  applyMode();
+};
+</script>
