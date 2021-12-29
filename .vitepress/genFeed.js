caf571ce89175cfa5a033a1b3c35cea58797ab2a
Michael Darko <mickdd22@gmail.com>
2021-12-29 20:51:23 +0000

    :wrench: removed rss loader

--- .vitepress/genFeed.js
+++ .vitepress/genFeed.js
@@ -15,29 +15,31 @@ const feed = new Feed({
   copyright: 'Copyright (c) 2021-present, Michael Darko and blog contributors'
 })
 
-load(true).forEach((post) => {
-  const file = path.resolve(__dirname, `dist${post.href}`)
-  const rendered = fs.readFileSync(file, 'utf-8')
-  const content = rendered.match(
-    /<div [^<>]+?class="prose[^<>]+?>([\s\S]*)<\/div><\/div><footer/
-  )
+// load(true).forEach((post) => {
+//   const file = path.resolve(__dirname, `dist${post.href}`)
+//   const rendered = fs.readFileSync(file, 'utf-8')
+//   const content = rendered.match(
+//     /<div [^<>]+?class="prose[^<>]+?>([\s\S]*)<\/div><\/div><footer/
+//   )
 
-  feed.addItem({
-    title: post.title,
-    id: `${url}${post.href}`,
-    link: `${url}${post.href}`,
-    description: post.excerpt,
-    content: content[1],
-    author: [
-      {
-        name: post.data.author,
-        link: post.data.twitter
-          ? `https://twitter.com/${post.data.twitter}`
-          : undefined
-      }
-    ],
-    date: post.data.date
-  })
-})
+//   console.log(content)
 
-fs.writeFileSync(path.resolve(__dirname, 'dist/feed.rss'), feed.rss2())
+//   feed.addItem({
+//     title: post.title,
+//     id: `${url}${post.href}`,
+//     link: `${url}${post.href}`,
+//     description: post.excerpt,
+//     content: content[1],
+//     author: [
+//       {
+//         name: post.data.author,
+//         link: post.data.twitter
+//           ? `https://twitter.com/${post.data.twitter}`
+//           : undefined
+//       }
+//     ],
+//     date: post.data.date
+//   })
+// })
+
+// fs.writeFileSync(path.resolve(__dirname, 'dist/feed.rss'), feed.rss2())
