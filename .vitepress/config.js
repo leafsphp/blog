66ab5508a0c0a5ed198ee540c68d4fab8a64a748
Michael Darko <mickdd22@gmail.com>
2022-06-01 00:41:49 +0000

    :sparkles: improved seo

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -11,6 +11,36 @@ export default defineConfig({
         type: 'image/x-icon',
         href: '/favicon.ico'
       }
+    ],
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content:
+          'leaf, leaf3, leaf php, leaf php framework, leaf blog, php, php5, php7, opensource, open-source, components, leaf tutorial, project'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'robots',
+        content: 'index, sollow, all'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'twitter:site',
+        content: '@leafphp'
+      }
+    ],
+    [
+      'meta',
+      {
+        name: 'og:image',
+        content:
+          'https://repository-images.githubusercontent.com/214705101/0ff19323-d2c5-46f5-a582-0b1f3a6eabcc'
+      }
     ]
   ],
   vite: {
